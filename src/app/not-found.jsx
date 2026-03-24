e33646f5020814fc07699c0f9db4a6920b5856b9
simeeram10-lgtm <simeeram10@gmail.com>
2026-03-24 20:16:20 +0530

    Remove custom not-found page

--- src/app/not-found.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: 40, textAlign: 'center' }}>
-      <h1 style={{ fontSize: 32, marginBottom: 16 }}>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
-  );
-}
