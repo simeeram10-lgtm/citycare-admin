6b364fcc3a09fc2bdae1e9659d62c7c537669ec8
simeeram10-lgtm <simeeram10@gmail.com>
2026-03-24 10:38:04 +0530

    Add custom client not-found page to fix Next.js build error

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1 style={{ fontSize: 32, marginBottom: 16 }}>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
